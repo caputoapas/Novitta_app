5c1a0a25bcfa95da5a8e0e25a08f6066fe39ba6f
Jefter <jefterallan@hotmail.com>
2018-04-22 22:14:48 -0300

    logica

--- app_novitta2/app/src/main/java/com/example/caputo/app_novitta/Main2Activity.java
+++ app_novitta2/app/src/main/java/com/example/caputo/app_novitta/Main2Activity.java
@@ -1,9 +1,9 @@
 package com.example.caputo.app_novitta;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.provider.BaseColumns;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -12,29 +12,91 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 
 public class Main2Activity extends AppCompatActivity {
     private ImageView imagem;
     private Button voltar;
     private TextView tipoVoto;
+    SimpleDateFormat formataData = new SimpleDateFormat("dd/MM/yyyy");
+    Date date = new Date();
+
+   final List<Pesquisa> Pessoas = new ArrayList<Pesquisa>();
+
+    TipoPessoa tp = null;
+
+    public enum TipoPessoa{
+        expositor,
+        visitante
+    }
+
+    public enum TipoSatisfacao{
+        satisfeito,
+        neutro,
+        insatisfeito
+    }
+
+    public class Pesquisa{
+        public TipoPessoa tipos;
+        public TipoSatisfacao satisfacao;
+        public String data;
+
+        public TipoSatisfacao getSatisfacao() {
+            return satisfacao;
+        }
+
+        public void setSatisfacao(TipoSatisfacao satisfacao) {
+            this.satisfacao = satisfacao;
+        }
+
+        public TipoPessoa getTipos() {
+            return tipos;
+        }
+
+        public void setTipos(TipoPessoa tipos) {
+            this.tipos = tipos;
+        }
+
+        public String getData() {
+            return data;
+        }
+
+        public void setData(String data) {
+            this.data = data;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        final Intent intent_v = getIntent();
+        Bundle parametro = intent_v.getExtras();
+
+        String r = parametro.getString("chave_nome");
+
+        if (r.equals("EXPOSITOR")) {
+            tp = TipoPessoa.expositor;
+        }else if (r.equals("VISITANTE"))
+            tp = TipoPessoa.visitante;
+        else
+            paginaInicial();
 
         tipoVoto = (TextView)findViewById(R.id.Tipo);
-        Intent intent_v = getIntent();
-        Bundle parametro = intent_v.getExtras();
-        if(parametro != null)
-        {
+
+        if(parametro != null) {
             String tipo = parametro.getString("chave_nome");
             //Toast.makeText(Main2Activity.this, tipo, Toast.LENGTH_SHORT).show();
             tipoVoto.setText(tipo);
-
         }
 
         voltar = (Button)findViewById(R.id.butonVoltar);
@@ -49,25 +111,60 @@ public class Main2Activity extends AppCompatActivity {
         imagem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                paginaInicial();
+                Satisfeito(v);
             }
         });
+
         imagem = (ImageView) findViewById(R.id.insatisfeito);
         imagem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                paginaInicial();
+                Insatisfeito(v);
             }
         });
+
         imagem = (ImageView) findViewById(R.id.neutro);
         imagem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                paginaInicial();
+                Neutro(v);
             }
         });
-
     }
+
+    public void Satisfeito (View v){
+        Pesquisa p = new Pesquisa();
+
+        p.setSatisfacao(TipoSatisfacao.satisfeito);
+        p.setTipos(tp);
+        p.setData(formataData.format(date));
+
+        Pessoas.add(p);
+        paginaInicial();
+    }
+
+    public void Insatisfeito(View v){
+        Pesquisa p = new Pesquisa();
+
+        p.setSatisfacao(TipoSatisfacao.insatisfeito);
+        p.setTipos(tp);
+        p.setData(formataData.format(date));
+
+        Pessoas.add(p);
+        paginaInicial();
+    }
+
+    public void Neutro(View v){
+        Pesquisa p = new Pesquisa();
+
+        p.setSatisfacao(TipoSatisfacao.neutro);
+        p.setTipos(tp);
+        p.setData(formataData.format(date));
+
+        Pessoas.add(p);
+        paginaInicial();
+    }
+
     public void paginaInicial(){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
@@ -106,5 +203,40 @@ public class Main2Activity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
+}
+    class CriaBanco extends SQLiteOpenHelper {
+    private static final String NOME_BANCO = "banco.db";
+    private static final String TABELA = "votacao";
+    private static final String ID = "_id";
+    private static final String TIPO = "tipo";
+    private static final String VOTO = "voto";
+    private static final String DATA = "data";
+    private static final int VERSAO = 1;
+
+    public CriaBanco(Context context){
+        super(context, NOME_BANCO,null,VERSAO);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE "+TABELA+"("
+                + ID + " integer primary key autoincrement,"
+                + TIPO + " text,"
+                + VOTO + " text,"
+                + DATA + " text"
+                +")";
+        db.execSQL(sql);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABELA);
+        onCreate(db);
+    }
+
 
 }
--- app_novitta2/app/src/main/java/com/example/caputo/app_novitta/MainActivity.java
+++ app_novitta2/app/src/main/java/com/example/caputo/app_novitta/MainActivity.java
@@ -2,8 +2,6 @@ package com.example.caputo.app_novitta;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -11,10 +9,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 
-import java.io.ObjectOutputStream;
-
 public class MainActivity extends AppCompatActivity {
     private ImageView imagem;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
                 paginaVotoV();
             }
         });
+
 
     }
 
@@ -93,7 +91,8 @@ public class MainActivity extends AppCompatActivity {
 
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
+
+
 }
